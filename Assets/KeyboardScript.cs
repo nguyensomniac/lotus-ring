3d6434a00e89732d567e886e41e7acc075ab1099
Lily Nguyen <online@lily.digital>
2017-12-05 23:45:46 -0800

    Add keyboard components

--- Assets/KeyboardScript.cs
+++ Assets/KeyboardScript.cs
@@ -8,10 +8,6 @@ public class KeyboardScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube.transform.position = new Vector3(0, 0, 5);
-        cube.transform.rotation = Quaternion.Euler(0, 45, 0);
-        cube.GetComponent<Renderer>().material.color = new Color(1F, 0F, 0F, 0.5F);
 	}
 	
 	// Update is called once per frame
---
+++ Assets/TextOption.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextOption : MonoBehaviour {
+
+    public string label;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(label);
+        Transform t = transform.Find("UITextPrefab").Find("Text");
+        UnityEngine.UI.Text text = t.gameObject.GetComponent<UnityEngine.UI.Text>();
+        if (text)
+        {
+            text.text = label;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
